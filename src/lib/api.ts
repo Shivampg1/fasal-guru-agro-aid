8722d4f5c35e44640afcb5a1bd3bb34c8d0c83c0
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-11-19 22:08:50 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE = "https://fca321a26ddb.ngrok-free.app";
+export const API_BASE = "https://2676bde8f77c.ngrok-free.app";
 
 // Enrolment (POST)
 export async function enrolFarmer(data: any) {
