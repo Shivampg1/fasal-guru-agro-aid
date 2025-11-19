b3ecd6bb0b07593589e5d273e8006e12d4f1306f
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-11-19 11:25:09 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,30 @@
+export const API_BASE = import.meta.env.VITE_API_URL;
+// Example: https://1d2503aaf8a4.ngrok-free.app
+
+export async function enrolFarmer(data: any) {
+  const res = await fetch(`${API_BASE}/enrolment/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+}
+
+export async function submitClaim(data: any) {
+  const res = await fetch(`${API_BASE}/claim/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+}
+
+export async function getYield(data: any) {
+  const res = await fetch(`${API_BASE}/yield/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+}
 
