180efd72e4746b91ff1cc12f16524a65dbfb08a9
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-08 23:31:10 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -116,8 +116,11 @@ const Dashboard = () => {
         </div>
         <div className="relative max-w-7xl mx-auto text-center">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
-            Welcome to <span className="text-harvest">FasalGuru</span>
+            Welcome to <span className="text-harvest">FASALGuru</span>
           </h1>
+          <h4 className="text-4xl md:text-6xl font-bold text-white mb-6">
+            Farm Advisory System for Agricultural Land
+          </h4>
           <p className="text-xl text-white/90 max-w-2xl mx-auto">
             Smart farming solutions for soil analysis, disease detection, weather
             prediction and PMFBY insurance services.
