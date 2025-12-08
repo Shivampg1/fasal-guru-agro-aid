a00a5893d9abc7c37271c022e6ae6bba72d3276a
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-08 15:02:13 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -55,15 +55,15 @@ const Dashboard = () => {
       onClick: () => window.open("https://weather-app-navy-nine-35.vercel.app", "_blank"),
     },
     
-  //   {
-  //   id: "dssat-yield",
-  //   title: "Yield Estimator (DSSAT)",
-  //   description: "Estimate crop yield using DSSAT-based models and farm data.",
-  //   icon: BarChart3, // or any other icon you prefer
-  //   gradient: "bg-gradient-to-br from-harvest to-primary",
-  //   buttonText: "Open DSSAT",
-  //   onClick: () => window.open("https://huggingface.co/spaces/cropdiseasedetection/dssat-detection-yield-prediction", "_blank"),
-  // },
+    {
+    id: "dssat-yield",
+    title: "Yield Estimator (DSSAT)",
+    description: "Estimate crop yield using DSSAT-based models and farm data.",
+    icon: BarChart3, // or any other icon you prefer
+    gradient: "bg-gradient-to-br from-harvest to-primary",
+    buttonText: "Open DSSAT",
+    onClick: () => window.open("https://huggingface.co/spaces/cropdiseasedetection/dssat-detection-yield-prediction", "_blank"),
+  },
     {
     id: "ndvi",
     title: "Satellite NDVI",
