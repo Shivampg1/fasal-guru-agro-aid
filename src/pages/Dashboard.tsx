dcec89323a3221673217c77c0d707bbc63d46e4c
Shivampg1 <147384496+Shivampg1@users.noreply.github.com>
2025-12-08 23:37:33 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -116,7 +116,7 @@ const Dashboard = () => {
         </div>
         <div className="relative max-w-7xl mx-auto text-center">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
-            Welcome to <span className="text-harvest">F.A.S.A.L Guru</span>
+            Welcome to <span className="text-harvest">F.A.S.A.L गुरु</span>
           </h1>
           <p className="text-4xl md:text-6xl text-white mb-6">
             (Farm Advisory System for Agricultural Land)
